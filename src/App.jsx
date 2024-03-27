2b9d3a32c9e78ec2499bca3add14e8b34716d88e
Lakshmi <kandelakshmi13@gmail.com>
2024-03-26 20:20:13 -0700

    modified login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../src/components/Login/Login";
+import Signup from './components/Signup/Signup';
+
 function App() {
   return (
-    <h1>Welcome</h1>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/Login" element={<Login />} />
+    <Route path="/Signup" element={<Signup />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
