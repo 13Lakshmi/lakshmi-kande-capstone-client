a8c73162c3a9ea033aa91027927e8a38362c1f46
Lakshmi <kandelakshmi13@gmail.com>
2024-03-26 20:11:44 -0700

    modified app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "../src/components/Login/Login";
-import Signup from './components/Signup/Signup';
-
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-    <Route path="/Login" element={<Login />} />
-    <Route path="/Signup" element={<Signup />} />
-    </Routes>
-    </BrowserRouter>
+    <h1>Welcome</h1>
   );
 }
 
